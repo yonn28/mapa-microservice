067d4bc7d152b36f764af62c24b0d25fd15c0eb7
namonroyr <namonroyr@unal.edu.co>
2021-09-02 16:35:48 -0500

    change of db and more columns for malnutrition percentage by dept

--- index.py
+++ index.py
@@ -5,22 +5,24 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 app = Flask(__name__)
 
-base_final = pd.read_csv('https://storage.googleapis.com/ds4all-test-bd1/base_final.csv')
+base_f = pd.read_csv('https://storage.googleapis.com/ds4all-test-bd1/base_target_final_180601.csv')
 
-dpts_count_target1=base_final.query("Target==1").groupby(['cod_dpto', 'nom_dpto','anno_encuesta', 'estrato']).size().to_frame('Count_Dpto_Target').reset_index()
-dpts_count_total=base_final.groupby(['cod_dpto', 'nom_dpto','anno_encuesta', 'estrato']).size().to_frame('Count_Dpto_Total').reset_index()
-dpts_count = pd.merge(dpts_count_target1, dpts_count_total, on=["cod_dpto", "nom_dpto"])
-dpts_count["count_ratio"] = dpts_count["Count_Dpto_Target"]/dpts_count["Count_Dpto_Total"]*100
+dpts_count_target1=base_f.query("Marca_Target_Reincidencia_F12M==1").groupby(['cod_dpto','nom_dpto','anno_encuesta']).size().to_frame('Count_Dpto_Rel').reset_index()
+dpts_count_target2=base_f.query("Marca_Target_EntroEnDesnutricion_F12M==1").groupby(['cod_dpto','nom_dpto','anno_encuesta']).size().to_frame('Count_Dpto_Mal').reset_index()
+dpts_count_total=base_f.groupby(['cod_dpto','nom_dpto','anno_encuesta']).size().to_frame('Count_Dpto_Total').reset_index()
+dpts_count_1 = pd.merge(dpts_count_target1, dpts_count_target2, on=["cod_dpto", "nom_dpto", "anno_encuesta"])
+dpts_count = pd.merge(dpts_count_1, dpts_count_total, on=["cod_dpto", "nom_dpto", "anno_encuesta"])
+dpts_count["Relapse_Percentage"] = dpts_count["Count_Dpto_Rel"]/dpts_count["Count_Dpto_Total"]*100
+dpts_count["Malnutrition_Percentage"] = dpts_count["Count_Dpto_Mal"]/dpts_count["Count_Dpto_Total"]*100
 dpts_count['cod_dpto']=pd.to_numeric(dpts_count['cod_dpto'])
 dpts_count['cod_dpto']=dpts_count['cod_dpto'].astype(int)
 dpts_count['cod_dpto']=dpts_count['cod_dpto'].apply(lambda x: '{0:0>2}'.format(x))
+dpts_count.rename(columns={'anno_encuesta': 'Año'}, inplace=True)
 
 
 @app.route('/api/v1/maps', methods=['GET'])
 def ploting_get_maps():
     return jsonify(dpts_count.to_dict("records"))
-
-
 
 
 if __name__ == '__main__':
